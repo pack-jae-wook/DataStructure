5cb28ac22477ffc1dc001f011e7b551dde1342b1
박재욱 <74747556+pack-jae-wook@users.noreply.github.com>
2021-03-10 16:58:45 +0900

    Add files via upload

---
+++ 202083285박재욱 0310/TwoDim.java
@@ -0,0 +1,15 @@
+package class01;
+
+public class TwoDim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int sale[][] = new int[][] {{63,84,140,130},
+    	{157,209,251,312}}; 
+     for(int i=0; i<2; i++) {
+    	 System.out.printf("%d/4ºÐ±â: sale[%d][%d]= %d %n",j+1,i,j,sale[i][j]);
+    	 System.out.println();
+     }
+	}
+
+}
